6da6615695a7c727e8363ccb383fe3fa6618c109
VaJoy Lan <vajoy@qq.com>
2025-06-06 02:42:09 +0800

    update: property

---
+++ temp/programming/packer-driver/targets/editor/chunks/35/35fd679b8365cfaf5b6035128ed5b1fdf34d17c9.js
@@ -0,0 +1,43 @@
+System.register(["cc"], function (_export, _context) {
+  "use strict";
+
+  var _cclegacy, __checkObsolete__, __checkObsoleteInNamespace__, _decorator, Component, Sprite, v4, _dec, _class, _crd, ccclass, property, SetColor;
+
+  return {
+    setters: [function (_cc) {
+      _cclegacy = _cc.cclegacy;
+      __checkObsolete__ = _cc.__checkObsolete__;
+      __checkObsoleteInNamespace__ = _cc.__checkObsoleteInNamespace__;
+      _decorator = _cc._decorator;
+      Component = _cc.Component;
+      Sprite = _cc.Sprite;
+      v4 = _cc.v4;
+    }],
+    execute: function () {
+      _crd = true;
+
+      _cclegacy._RF.push({}, "ce3481lXWZCRo7ouJZLulog", "SetColor", undefined);
+
+      __checkObsolete__(['_decorator', 'Component', 'Sprite', 'v4']);
+
+      ({
+        ccclass,
+        property
+      } = _decorator);
+
+      _export("SetColor", SetColor = (_dec = ccclass('SetColor'), _dec(_class = class SetColor extends Component {
+        start() {
+          const sp = this.getComponent(Sprite);
+          const mat = sp.material;
+          mat.setProperty('coverColor', v4(1.0, 0.0, 0.0, 1.0));
+        }
+
+      }) || _class));
+
+      _cclegacy._RF.pop();
+
+      _crd = false;
+    }
+  };
+});
+//# sourceMappingURL=35fd679b8365cfaf5b6035128ed5b1fdf34d17c9.js.map
---
+++ temp/programming/packer-driver/targets/preview/chunks/b1/b1f880621fabecc79b747cb3252b8b18bb5029f5.js
@@ -0,0 +1,12 @@
+System.register([], function (_export, _context) {
+  "use strict";
+
+  return {
+    setters: [],
+    execute: function () {
+      // This module is auto-generated to report error emitted when try to load module file:///Users/vajoylan/Documents/GitHub/shader/assets/basic/3-opaque-attribute/SetColor.ts at runtime.
+      throw new Error("SyntaxError: /file:/Users/vajoylan/Documents/GitHub/shader/assets/basic/3-opaque-attribute/SetColor.ts: Unexpected token (9:4)\n\n   7 |         const sp = this.getComponent(\"cc.Sprite\");\n   8 |         const mat = sp.\n>  9 |     }\n     |     ^\n  10 |\n  11 |     update(deltaTime: number) {\n  12 |         ");
+    }
+  };
+});
+//# sourceMappingURL=b1f880621fabecc79b747cb3252b8b18bb5029f5.js.map
